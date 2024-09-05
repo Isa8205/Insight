ee38c0b226b3125b2609af690aeb1a4d6f62b72e
Isa8205 <156502124+Isa8205@users.noreply.github.com>
2024-09-05 20:14:17 +0300

    Bug fixes

--- Insight/settings.py
+++ Insight/settings.py
@@ -95,6 +95,7 @@ if ENVIRONMENT == 'developement':
         }
     }
 else:
+    DEBUG = False
     DATABASES = {
         'default': dj_database_url.config(
             default=os.getenv('DATABASE_URL')
